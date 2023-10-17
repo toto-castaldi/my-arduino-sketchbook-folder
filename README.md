a67c7dfe8cb8734ffe5fbe52f3af9c6f8d50d1e4
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2023-10-17 18:29:49 +0200

    i2c led

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 Arduino stuff
 
 thanks to [Thinkercad](https://www.tinkercad.com/)
+
+# NOTES
+
+## LED
+
+An LED has two pins. Anode and Cathode: the first is designed with a skew leg (in real life, it is longer) and is plugged into the power; the second is straight (shorter) and is linked to the ground.
---
+++ i2c-led/sketch/sketch.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin();
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("ok");
+}
+
+void loop()
+{
+  Serial.println("on");
+  Wire.beginTransmission(0x20);
+  Wire.write((byte)0b11111111);
+  Wire.endTransmission();
+
+  delay(500);
+
+  Serial.println("off");
+  Wire.beginTransmission(0x20);
+  Wire.write((byte)0b00000000);
+  Wire.endTransmission();
+
+  delay(500);
+}
