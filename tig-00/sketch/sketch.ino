99b94d96e4bb5052fdb5afbcd03bde35fe6e4adc
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2024-02-09 18:13:06 +0100

    player waiting timeout

--- tig-00/sketch/sketch.ino
+++ tig-00/sketch/sketch.ino
@@ -25,21 +25,17 @@ enum buttons {
               BUTTON_YELLOW,
               BUTTON_GREEN,
               BUTTON_BLUE,
-              BUTTON_RED
+              BUTTON_RED,
+              BUTTON_NO //null ????
             };
 
 int tones[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
 //            mid C  C#   D    D#   E    F    F#   G    G#   A
 int level;
-int gameSequence[100];  //100 is the maximum level *TODO: remove*
-                        //0 - NO COLOR
-                        //1 YELLOW
-                        //2 GREEN
-                        //3 BLUE
-                        //4 RED
+buttons gameSequence[100];  //100 is the maximum level *TODO: remove*
 gameStates gameState;
 
-buttons lobbyLed;
+buttons animationButton;
 
 int presentingIndex;
 int playerPlayingIndex;
@@ -49,8 +45,7 @@ bool yBR, gBR, bBR, rBR;
 
 bool needWait;
 
-unsigned long timerPlaying;
-unsigned long timerPause;
+unsigned long timerPlaying, timerPause, timerPlayerWaiting;
 
 void setup() {
   Wire.begin();
@@ -75,24 +70,7 @@ void loop() {
   switch (gameState) {
     case LOBBY:
       if (playingPassed()) {
-        if (lobbyLed == BUTTON_YELLOW) lobbyLed = BUTTON_GREEN;
-        else if (lobbyLed == BUTTON_GREEN) lobbyLed = BUTTON_RED;
-        else if (lobbyLed == BUTTON_RED) lobbyLed = BUTTON_BLUE;
-        else if (lobbyLed == BUTTON_BLUE) lobbyLed = BUTTON_YELLOW;
-        switch (lobbyLed) {
-          case BUTTON_YELLOW:
-              yON(false);
-            break;
-          case BUTTON_GREEN:
-              gON(false);
-            break;
-          case BUTTON_BLUE:
-              bON(false);
-            break;
-          case BUTTON_RED:
-              rON(false);
-            break;
-        }
+        rotateAnimationButton();
       }
       readPlayingButtons();
       if (yB || gB || bB || rB) {
@@ -102,12 +80,14 @@ void loop() {
     break;
     case SEQUENCE_CREATE_UPDATE:
       Serial.print("level ");
-      Serial.println(level);
-      for (int n = level - 1; n < sizeof(gameSequence)/sizeof(int); n++) {
+      Serial.print(level);
+      Serial.print(" - ");
+      Serial.println(500 - penalty(500) + 100);
+      for (int n = level - 1; n < 100; n++) { //100 !!!
         if (n < level) {
-          gameSequence[n] = random(0, 4) + 1;
+          gameSequence[n] = randomButton();
         } else {
-          gameSequence[n] = 0; //clear prev sequence
+          gameSequence[n] = BUTTON_NO; //clear prev sequence
         }
       }
       changeGameState(SEQUENCE_PRESENTING);
@@ -117,24 +97,24 @@ void loop() {
       if (playingPassed() && !needWait) {
         if (pausePassed()) {
           presentingIndex ++;
-          if (gameSequence[presentingIndex] > 0) {
+          if (gameSequence[presentingIndex] != BUTTON_NO) {
             switch (gameSequence[presentingIndex]) {
-              case 1:
+              case BUTTON_YELLOW:
                   Serial.println("Y");
                   yON();
                   needWait = true;
                 break;
-              case 2:
+              case BUTTON_GREEN:
                   Serial.println("G");
                   needWait = true;
                   gON();
                 break;
-              case 3:
+              case BUTTON_BLUE:
                   Serial.println("B");
                   needWait = true;
                   bON();
                 break;
-              case 4:
+              case BUTTON_RED:
                   Serial.println("R");
                   needWait = true;
                   rON();
@@ -142,6 +122,7 @@ void loop() {
             }
           } else {
             changeGameState(PLAYER_WAITING);
+            playerWaitingStart();
             playerPlayingIndex = 0;    
           }
         }
@@ -152,54 +133,65 @@ void loop() {
       }
       break;
       case PLAYER_WAITING:
-        readPlayingButtons();
-        if (playingPassed() || yB || gB || bB || rB) {
-          stopLeds();
-          if (gameSequence[playerPlayingIndex] == 0) {
-            Serial.println("New Level");
-            delay(500);
-            level ++;
-            changeGameState(SEQUENCE_CREATE_UPDATE);
-          } else {    
-            if (yB) {
-              if (gameSequence[playerPlayingIndex] == 1) {
-                Serial.print(playerPlayingIndex);
-                Serial.println(" - Y OK");
-                yON();
-                playerPlayingIndex ++;
-              } else {
-                Serial.println("Y KO");
-                changeGameState(GAME_OVER);
-              }
-            } else if (gB) {
-              if (gameSequence[playerPlayingIndex] == 2) {
-                Serial.print(playerPlayingIndex);
-                Serial.println(" - G OK");
-                gON();
-                playerPlayingIndex ++;
-              } else {
-                Serial.println("G KO");
-                changeGameState(GAME_OVER);
-              }
-            } else if (bB) {
-              if (gameSequence[playerPlayingIndex] == 3) {
-                Serial.print(playerPlayingIndex);
-                Serial.println(" - B OK");
-                bON();
-                playerPlayingIndex ++;
-              } else {
-                Serial.println("B KO");
-                changeGameState(GAME_OVER);
-              }
-            } else if (rB) {
-              if (gameSequence[playerPlayingIndex] == 4) {
-                Serial.print(playerPlayingIndex);
-                Serial.println(" - R OK");
-                rON();
-                playerPlayingIndex ++;
-              } else {
-                Serial.println("R KO");
-                changeGameState(GAME_OVER);
+        if (playerWaitingTimeOut()) {
+          Serial.println("Player TIMEOUT");
+          playerTimeOutEffect();
+          delay(1000);
+          changeGameState(GAME_OVER);
+        } else {
+          readPlayingButtons();
+          if (playingPassed() || yB || gB || bB || rB) {
+            stopLeds();
+            if (gameSequence[playerPlayingIndex] == BUTTON_NO) {
+              Serial.println("New Level");
+              delay(500);
+              level ++;
+              changeGameState(SEQUENCE_CREATE_UPDATE);
+            } else {    
+              if (yB) {
+                if (gameSequence[playerPlayingIndex] == BUTTON_YELLOW) {
+                  Serial.print(playerPlayingIndex);
+                  Serial.println(" - Y OK");
+                  playerWaitingStart();
+                  yON();
+                  playerPlayingIndex ++;
+                } else {
+                  Serial.println("Y KO");
+                  changeGameState(GAME_OVER);
+                }
+              } else if (gB) {
+                if (gameSequence[playerPlayingIndex] == BUTTON_GREEN) {
+                  Serial.print(playerPlayingIndex);
+                  Serial.println(" - G OK");
+                  playerWaitingStart();
+                  gON();
+                  playerPlayingIndex ++;
+                } else {
+                  Serial.println("G KO");
+                  changeGameState(GAME_OVER);
+                }
+              } else if (bB) {
+                if (gameSequence[playerPlayingIndex] == BUTTON_BLUE) {
+                  Serial.print(playerPlayingIndex);
+                  Serial.println(" - B OK");
+                  playerWaitingStart();
+                  bON();
+                  playerPlayingIndex ++;
+                } else {
+                  Serial.println("B KO");
+                  changeGameState(GAME_OVER);
+                }
+              } else if (rB) {
+                if (gameSequence[playerPlayingIndex] == BUTTON_RED) {
+                  Serial.print(playerPlayingIndex);
+                  Serial.println(" - R OK");
+                  playerWaitingStart();
+                  rON();
+                  playerPlayingIndex ++;
+                } else {
+                  Serial.println("R KO");
+                  changeGameState(GAME_OVER);
+                }
               }
             }
           }
@@ -211,6 +203,14 @@ void loop() {
         reset();
       break;
   }
+}
+
+buttons randomButton() {
+  int r = random(0, 4) + 1;
+  if (r == 1) return BUTTON_YELLOW;
+  if (r == 2) return BUTTON_GREEN;
+  if (r == 3) return BUTTON_BLUE;
+  if (r == 4) return BUTTON_RED;
 }
 
 void readPlayingButtons() {
@@ -260,12 +260,46 @@ void readPlayingButtons() {
   }
 }
 
+void rotateAnimationButton() {
+  if (animationButton == BUTTON_YELLOW) animationButton = BUTTON_GREEN;
+  else if (animationButton == BUTTON_GREEN) animationButton = BUTTON_RED;
+  else if (animationButton == BUTTON_RED) animationButton = BUTTON_BLUE;
+  else if (animationButton == BUTTON_BLUE) animationButton = BUTTON_YELLOW;
+  switch (animationButton) {
+    case BUTTON_YELLOW:
+        yON(false);
+      break;
+    case BUTTON_GREEN:
+        gON(false);
+      break;
+    case BUTTON_BLUE:
+        bON(false);
+      break;
+    case BUTTON_RED:
+        rON(false);
+      break;
+  }
+}
+
+int penalty (int base) {
+  double difficulty = (double)-1/((double)level * (double)level) + 0.5;
+  int penalty = 0;
+  if (difficulty > 0) {
+    penalty += (int) floor(difficulty * base);
+  }
+  return penalty;
+}
+
+bool playerWaitingTimeOut() {
+  return millis() - timerPlayerWaiting >= 5000;
+}
+
 bool playingPassed() {
-  return millis() - timerPlaying >= 500;
+  return millis() - timerPlaying >= 500 - penalty(400);
 }
 
 bool pausePassed() {
-  return millis() - timerPause >= 300;
+  return millis() - timerPause >= 300 - penalty(200);
 }
 
 void pauseStart() {
@@ -276,6 +310,10 @@ void playingStart() {
   timerPlaying = millis();
 }
 
+void playerWaitingStart() {
+  timerPlayerWaiting = millis();
+}
+
 void reset() {
   needWait = false;
   presentingIndex = -1;
@@ -284,9 +322,7 @@ void reset() {
   gameState = LOBBY;
   yB, gB, bB, rB = false;
   yBR, gBR, bBR, rBR = true;
-  lobbyLed = BUTTON_YELLOW;
-  pauseStart();
-  playingStart();
+  animationButton = BUTTON_YELLOW;
 }
 
 
@@ -305,6 +341,14 @@ void ledOn(byte led, int soundIndex){
   if (soundIndex > -1) {
     tone(PIN_SPEAKER, tones[soundIndex]);
   }
+  playingStart();
+}
+
+void playerTimeOutEffect() {
+  Wire.beginTransmission(0x20);
+  Wire.write((byte)0b11110000);
+  Wire.endTransmission();
+  tone(PIN_SPEAKER, tones[4]);
   playingStart();
 }
 
@@ -341,13 +385,14 @@ void gameOver() {
  for (int thisNote = 0; thisNote < 8; thisNote++) {
     int noteDuration = 1000/noteDurations[thisNote];
     tone(PIN_SPEAKER, melody[thisNote],noteDuration);
-    if (thisNote % 4 == 0) yON(false); 
-    if (thisNote % 4 == 1) gON(false); 
-    if (thisNote % 4 == 2) bON(false); 
-    if (thisNote % 4 == 3) rON(false);
+    if (melody[thisNote] > 0) {
+      rotateAnimationButton();
+    }
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     noTone(PIN_SPEAKER);
-    stopLeds();
+    if (melody[thisNote] > 0) {
+      stopLeds();
+    }
   }
 }
